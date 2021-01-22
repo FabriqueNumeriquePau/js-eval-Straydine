06a4be28f6715e1e761ac6ee2d462e13a440c30a
Straydine <aiguadelstraydine@gmail.com>
2021-01-22 12:11:00 +0100

    eval

--- js/dojosjs.js
+++ js/dojosjs.js
@@ -1,1 +1,18 @@
 console.log("Go Chuck !");
+let gochuck = document.getElementById("Go Chuck !");
+
+gochuck.addEventListener("mouseenter", function( event ) {
+    event.target.style.animate = "square";
+    setTimeout(function() {
+        event.target.style.animate = "square";
+      }, 500);
+    }, false);
+    
+    
+console.log("fleche");
+let fleche = document.nextElementSibling("fleche");
+
+function 
+
+    
+}
